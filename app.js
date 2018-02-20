5dcdce3572fd0213219e50b7f87338061d0498dd
Daniel Sanchez <atlow.archive@gmail.com>
2018-02-19 17:42:31 -0800

    [setup] prompting user for square

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
 const prompt = require('prompt');
+prompt.start();
 
 const logBoard = (board) => {
   console.log('Current board');
-  console.log(board);
+  console.log(`[${board[0][0]}, ${board[0][1]}, ${board[0][2]}]`);
+  console.log(`[${board[1][0]}, ${board[1][1]}, ${board[1][2]}]`);
+  console.log(`[${board[2][0]}, ${board[2][1]}, ${board[2][2]}]`);
 };
 
-const startGame = () => {
-  const board = [['', '', ''], ['', '', ''], ['', '', '']];
-  const currentPlayer = 'X';
+const pickSquare = (player) => {
+  let square;
+  const spot = {
+    properties: {
+      square: {
+        description: `Player ${player}, please choose a square`
+      } 
+    }
+  };
+
+  prompt.get(spot, function (err, result) {
+    square = result.square.toString();
+    console.log(square);
+  });
+
+  return square;
+};
+
+
+const startGame = (player, foundWinner) => {
+  const board = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']];
+  // while no current winner keep playing
+  const currentPlayer = player === 'X' ? 'O' : 'X';
   console.log(`Player ${currentPlayer}'s turn`);
   logBoard(board);
+  const square = pickSquare(currentPlayer);
+  console.log('this is the square: ', square);
 };
 
-// prompt.get(['firstName', 'lastName'], function (err, result) {
-//   console.log('Thank you.');
-//   console.log('first: ', result.firstName);
-//   console.log('last: ', result.lastName);
-//   return result.firstName;
-// });
 
-startGame();
+startGame('O', false);
